fb47adecaf9f429ce7405127f078462368ec22a3
JulioAlb453 <fuliobeto18@gmail.com>
2024-09-26 09:35:35 -0500

    update: card-carousel, golden-era, navbar, succesful-games
    se implemento la informacion sobre el tema a abordar, igualmente se cambiaron algunos estilos

--- src/app/card-carousel/card-carousel.component.ts
+++ src/app/card-carousel/card-carousel.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Importa CommonModule
 
 @Component({
   selector: 'app-card-carousel',
   standalone: true,
-  imports: [],
+  imports: [CommonModule], // Agrega CommonModule aquí
   templateUrl: './card-carousel.component.html',
-  styleUrl: './card-carousel.component.css'
+  styleUrls: ['./card-carousel.component.css']
 })
 export class CardCarouselComponent {
-
+  @Input() cards: { title: string; imageUrl: string; description: string; }[] = [];
 }
--- src/app/golden-era/golden-era.component.ts
+++ src/app/golden-era/golden-era.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-golden-era',
   standalone: true,
-  imports: [],
   templateUrl: './golden-era.component.html',
-  styleUrl: './golden-era.component.css'
+  styleUrls: ['./golden-era.component.css']
 })
 export class GoldenEraComponent {
-
+  @Input() title: string = '';
+  @Input() description: string = '';
+  @Input() imageUrl: string = '';
 }
